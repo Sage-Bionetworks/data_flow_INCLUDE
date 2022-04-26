c00e2c102eeb204e5c95aa714cc521286810f74d
lakikowolfe <lauren.wolfe@sagebase.org>
2022-04-26 12:04:16 -0700

    connect mod_dataset_selection ui to backend to schematic api calls

--- R/app_server.R
+++ R/app_server.R
@@ -5,7 +5,24 @@
 #' @import shiny
 #' @noRd
 app_server <- function( input, output, session ) {
-  # Your application server logic 
-  ds <- mod_dataset_selection_server("dataset_selection_ui_1")
-  mod_file_selection_server("file_selection_ui_1", ds)
+  # Your application server logic
+  
+  # DEV STUFF #########################################################################
+  
+  # SYNAPSE LOGIN
+  # TODO: log in will eventually live in global.R/rely on config info
+  reticulate::use_virtualenv(".venv/")
+  synapseclient <- reticulate::import("synapseclient")
+  syntab <- reticulate::import("synapseclient.table")
+  syn <- synapseclient$Synapse()
+  syn$login()
+
+  # APP SERVER LOGIC  ##############################################################
+  
+  # MOD_DATASET_SELECTION
+  # input: dataframe of storage projects (name, id)
+  mod_dataset_selection_server("dataset_selection_ui_1")
+  
+  #MOD_FILE_SELECTION
+  #mod_file_selection_server("file_selection_ui_1", ds)
 }
--- R/mod_dataset_selection.R
+++ R/mod_dataset_selection.R
@@ -1,4 +1,4 @@
-#' dataset_selection UI Function
+#' dataset_selection2 UI Function
 #'
 #' @description A shiny Module.
 #'
@@ -11,63 +11,132 @@ mod_dataset_selection_ui <- function(id){
   ns <- NS(id)
   tagList(
     
-    # select dataset box
+    ## SELECT PROJECT BOX  ####################################################
+    
+    shinydashboard::box(
+      title = "Select Project",
+      width = 6,
+      
+      # Project dropdown
+      uiOutput(ns("project_selector")),
+      
+      # Button to initiate project selection
+      actionButton(ns("select_project_btn"),
+                   "Submit"),
+      br(),
+      
+    ),
+    
+    ## SELECT DATASET BOX  ####################################################
+    
     shinydashboard::box(
       title = "Select Dataset",
       width = 6,
       
-      # output table
-      DT::DTOutput(ns("tbl")),
+      # Table of storage project datasets 
+      DT::DTOutput(ns("dataset_tbl")),
       
       br(),
       
-      # selection action button
-      actionButton(ns("button"), "Select Dataset(s)"),
+      # Button to initiate dataset selection
+      actionButton(ns("select_dataset_btn"), "Select Dataset(s)"),
       
-      br(),
-      )
+      br()
     )
+ 
+  )
 }
     
-#' dataset_selection Server Functions
+#' dataset_selection2 Server Functions
 #'
 #' @noRd 
-mod_dataset_selection_server <- function(id) {
-  
-  moduleServer( id, function(input, output, session) {
-    
+mod_dataset_selection_server <- function(id){
+  moduleServer( id, function(input, output, session){
     ns <- session$ns
     
-    # create dataset datatable
-    # create dummy table
-    # this will be a call to synapse to get datasets in a specified fileview
-    datasets_col <- paste0(rep("dataset", 200), "_", seq(1:200))
-    status_col <- sample(c("Quarantine", "Release 1"), 200, replace = TRUE)
-    dummy_data <- data.frame(Dataset = datasets_col, Status = as.factor(status_col))
+    # HARDCODED VARIABLES ###################################################################################
     
-    # render data table with scroll bar, no pagination, and filtering
-    output$tbl <- DT::renderDT({
-      DT::datatable(dummy_data,
-                    option = list(scrollY = 500,
-                                  scrollCollapse = TRUE,
-                                  bPaginate = FALSE,
-                                  dom = "t"),
-                    filter = list(position = 'top', clear = TRUE))
+    # FIXME: Need to load data before app launches (like how DCA waites for data before loading)
+    # Currently it takes the drop down a loooong time to render and appears to be missing.
+    
+    # get token from renviron
+    # TODO: repetitive figure out how to store or pass as variable
+    
+    schematic_token <- Sys.getenv("schematicToken")
+    
+    # manually set asset view
+    # TODO: repetitive figure out how to store or pass asset_view
+    
+    asset_view <- "syn20446927"
+    
+    # API CALL : GET STORAGE PROJECTS #######################################################################
+    storage_projects_list <- storage_projects(asset_view = asset_view,
+                                              input_token = schematic_token)
+    
+    # get names from list
+    storage_projects_names <- purrr::map_chr(storage_projects_list, purrr::pluck(2))
+    storage_projects_ids <- purrr::map_chr(storage_projects_list, purrr::pluck(1))
+    storage_projects_df <- data.frame(name = storage_projects_names, 
+                                      id = storage_projects_ids)
+
+    
+    # DROP DOWN LISTING STORAGE PROJECTS ####################################################################
+
+    output$project_selector <- renderUI({
+      selectInput(session$ns("selected_projects"),
+                  label = "Select Project",
+                  choices = storage_projects_df$name)
     })
-      
     
-    # when button is pushed
-    # if no rows selected: show no dataset selected
-    # if rows selected return the selection
-    eventReactive(input$button, {
-      s <- input$tbl_rows_selected
-      if (length(s) == 0) {
-        showNotification("No Dataset Selected")
-        return(NULL)
-      } else{
-        return(dummy_data[s,])
-        }
+    ## ON CLICK DISPLAY STORAGE PROJECT DATASETS  ###########################################################
+    # on button click call storage_project_datasets using selected project ID
+    
+    observeEvent(input$select_project_btn, {
+
+      # subset storage project df by selected project
+      # TODO: allow multiple project selection?
+
+      selected_project_df <- storage_projects_df[grepl(input$selected_projects, storage_projects_df$name), ]
+
+      # call schematic API - get datasets for selected storage project
+
+      dataset_list <- storage_project_datasets(asset_view = asset_view,
+                                               project_id = selected_project_df$id,
+                                               input_token = schematic_token)
+
+      # schematic outputs a list
+      # parse into a dataframe
+
+      dataset_df <- data.frame(do.call(rbind, dataset_list))
+      names(dataset_df) <- c("ids", "name")
+      dataset_df <- dplyr::select(dataset_df, name, ids)
+
+      # render data table with scroll bar, no pagination, and filtering
+      output$dataset_tbl <- DT::renderDataTable({
+        DT::datatable(dataset_df)
       })
     })
-  }
-  
+
+    # ## ON BUTTON CLICK SUBMIT DATASET SELECTION #############################################################
+    # 
+    # # when button is pushed
+    # # if no rows selected: show no dataset selected
+    # # if rows selected return the selection
+    # eventReactive(input$select_dataset_btn, {
+    #   s <- input$dataset_tbl_rows_selected
+    #   if (length(s) == 0) {
+    #     showNotification("No Dataset Selected")
+    #     return(NULL)
+    #   } else{
+    #     return(dataset_df[s,])
+    #     }
+    #   })
+ 
+  })
+}
+    
+## To be copied in the UI
+# mod_dataset_selection2_ui("dataset_selection2_1")
+    
+## To be copied in the server
+# mod_dataset_selection2_server("dataset_selection2_1")
