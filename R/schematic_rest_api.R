a921d7af3b58b3a620b876a93cf424f716b6cce7
lakikowolfe <lauren.wolfe@sagebase.org>
2022-04-28 11:47:17 -0700

    add json param to manifest/download, update roxygen comments

--- R/schematic_rest_api.R
+++ R/schematic_rest_api.R
@@ -1,4 +1,4 @@
-### FUNCTIONS FROM DATA_CURATOR/R/SCHEMATIC_REST_API.R by @AFWILLIA
+### FUNCTIONS MOSTLY FROM DATA_CURATOR/R/SCHEMATIC_REST_API.R by @AFWILLIA
 
 ## MANIFEST OPERATIONS #########################################################
 
@@ -8,50 +8,50 @@
 #' @param asset_view ID of view listing all project data assets. For example, for Synapse this would be the Synapse ID of the fileview listing all data assets for a given project.(i.e. master_fileview in config.yml)
 #' @param dataset_id Synapse ID of existing manifest
 #' @param input_token Synapse login cookie, PAT, or API key.
+#' @param as_json Synapse login cookie, PAT, or API key.
 #' 
 #' @returns a URL to a google sheet
 #' @export
 
-manifest_download <- function(url="http://localhost:3001/v1/manifest/download",
-                              asset_view,
+manifest_download <- function(asset_view,
                               dataset_id,
-                              input_token) {
-  # define headers
+                              input_token,
+                              as_json,
+                              url="http://localhost:3001/v1/manifest/download") {
   headers = c(
-    `accept` = 'text/csv'
+    `accept` = 'application/json'
   )
   
-  # define params
   params = list(
     `input_token` = input_token,
     `asset_view` = asset_view,
-    `dataset_id` = dataset_id
+    `dataset_id` = dataset_id,
+    `as_json` = as_json
   )
   
-  # make request
-  req <- httr::GET(url = url,
-                   httr::add_headers(.headers=headers), 
-                   query = params)
-  
-  # parse request
-  httr::content(req, as="text")
+  res <- httr::GET(url = url, httr::add_headers(.headers=headers), query = params)
 }
 
 #' schematic rest api to generate manifest
 #'
 #' @param title Name of dataset 
 #' @param data_type Type of dataset 
+#' @param dataset_id Synapse ID of existing manifest
+#' @param url
+#' @param schema_url
 #' @param oauth true or false STRING passed to python
 #' @param use_annotations true or false STRING passed to python 
-#' @param dataset_id Synapse ID of existing manifest
 #' 
 #' @returns a URL to a google sheet
 #' @export
 
-manifest_generate <- function(url="http://localhost:3001/v1/manifest/generate",
+manifest_generate <- function(title, 
+                              data_type,
+                              dataset_id,
+                              url="http://localhost:3001/v1/manifest/generate",
                               schema_url="https://raw.githubusercontent.com/ncihtan/data-models/main/HTAN.model.jsonld", #nolint
-                              title, data_type, oauth="true",
-                              use_annotations="false", dataset_id) {
+                              oauth="true",
+                              use_annotations="false") {
   
   req <- httr::GET(url,
                    query = list(
@@ -68,17 +68,19 @@ manifest_generate <- function(url="http://localhost:3001/v1/manifest/generate",
 }
 
 #' Populate a manifest sheet
-#' 
-#' @param url URL to schematic API endpoint
-#' @param schema_url URL to a schema jsonld 
+#'
 #' @param data_type Type of dataset
 #' @param title Title of csv
 #' @param csv_file Filepath of csv to validate
+#' @param url URL to schematic API endpoint
+#' @param schema_url URL to a schema jsonld 
 #' @export
 
-manifest_populate <- function(url="http://localhost:3001/v1/manifest/populate",
-                              schema_url="https://raw.githubusercontent.com/ncihtan/data-models/main/HTAN.model.jsonld", #notlint
-                              data_type, title, csv_file) {
+manifest_populate <- function(data_type, 
+                              title, 
+                              csv_file,
+                              url="http://localhost:3001/v1/manifest/populate",
+                              schema_url="https://raw.githubusercontent.com/ncihtan/data-models/main/HTAN.model.jsonld") {
   
   req <- httr::POST(url,
                     query=list(
@@ -95,17 +97,19 @@ manifest_populate <- function(url="http://localhost:3001/v1/manifest/populate",
 
 #' schematic rest api to validate metadata
 #' 
-#' @param url URL to schematic API endpoint
-#' @param schema_url URL to a schema jsonld 
 #' @param data_type Type of dataset
 #' @param csv_file Filepath of csv to validate
+#' @param url URL to schematic API endpoint
+#' @param schema_url URL to a schema jsonld 
 #' 
-#' @returns An empty list() if sucessfully validated. Or a list of errors.
+#' @returns An empty list() if successfully validated. Or a list of errors.
 #' @export
 
-manifest_validate <- function(url="http://localhost:3001/v1/model/validate",
-                              schema_url="https://raw.githubusercontent.com/ncihtan/data-models/main/HTAN.model.jsonld", #nolint
-                              data_type, csv_file) {
+manifest_validate <- function(data_type, 
+                              csv_file,
+                              url="http://localhost:3001/v1/model/validate",
+                              schema_url="https://raw.githubusercontent.com/ncihtan/data-models/main/HTAN.model.jsonld") {
+  
   req <- httr::POST(url,
                     query=list(
                       schema_url=schema_url,
@@ -120,19 +124,22 @@ manifest_validate <- function(url="http://localhost:3001/v1/model/validate",
 
 #' schematic rest api to submit metadata
 #' 
-#' @param url URL to schematic API endpoint
-#' @param schema_url URL to a schema jsonld 
 #' @param data_type Type of dataset
 #' @param dataset_id Synapse ID of existing manifest
 #' @param input_token Synapse login cookie, PAT, or API key.
 #' @param csv_file Filepath of csv to validate
+#' @param url URL to schematic API endpoint
+#' @param schema_url URL to a schema jsonld 
 #' 
 #' @returns TRUE if successful upload or validate errors if not.
 #' @export
 
-model_submit <- function(url="http://localhost:3001/v1/model/submit",
-                         schema_url="https://raw.githubusercontent.com/ncihtan/data-models/main/HTAN.model.jsonld", #notlint
-                         data_type, dataset_id, input_token, csv_file) {
+model_submit <- function(data_type, 
+                         dataset_id, 
+                         input_token, 
+                         csv_file,
+                         url="http://localhost:3001/v1/model/submit",
+                         schema_url="https://raw.githubusercontent.com/ncihtan/data-models/main/HTAN.model.jsonld") {
   req <- httr::POST(url,
                     #add_headers(Authorization=paste0("Bearer ", pat)),
                     query=list(
@@ -151,18 +158,17 @@ model_submit <- function(url="http://localhost:3001/v1/model/submit",
 
 #' Gets all datasets in folder under a given storage project that the current user has access to.
 #' 
-#' @param url URL to schematic API endpoint
-#' @param syn_master_file_view synapse ID of master file view.
-#' @param syn_master_file_name Synapse storage manifest file name.
+#' @param asset_view synapse ID of master file view.
 #' @param project_id synapse ID of a storage project.
 #' @param input_token synapse PAT
+#' @param url URL to schematic API endpoint
 #'
 #'@export
 
-storage_project_datasets <- function(url="http://localhost:3001/v1/storage/project/datasets",
-                                     asset_view,
+storage_project_datasets <- function(asset_view,
                                      project_id,
-                                     input_token) {
+                                     input_token,
+                                     url="http://localhost:3001/v1/storage/project/datasets") {
   
   req <- httr::GET(url,
                    #add_headers(Authorization=paste0("Bearer ", pat)),
@@ -177,16 +183,15 @@ storage_project_datasets <- function(url="http://localhost:3001/v1/storage/proje
 
 #' Get all storage projects the current user has access to
 #' 
-#' @param url URL to schematic API endpoint
-#' @param syn_master_file_view synapse ID of master file view.
-#' @param syn_master_file_name Synapse storage manifest file name.
+#' @param asset_view synapse ID of master file view.
 #' @param input_token synapse PAT
+#' @param url URL to schematic API endpoint
 #'
 #' @export
 
-storage_projects <- function(url="http://localhost:3001/v1/storage/projects",
-                             asset_view,
-                             input_token) {
+storage_projects <- function(asset_view,
+                             input_token,
+                             url="http://localhost:3001/v1/storage/projects") {
   
   req <- httr::GET(url,
                    query = list(
@@ -199,20 +204,21 @@ storage_projects <- function(url="http://localhost:3001/v1/storage/projects",
 
 #' /storage/dataset/files
 #'
-#' @param url URL to schematic API endpoint
-#' @param syn_master_file_view synapse ID of master file view.
-#' @param syn_master_file_name Synapse storage manifest file name.
+#' @param asset_view synapse ID of master file view.
 #' @param dataset_id synapse ID of a storage dataset.
+#' @param input_token synapse PAT
 #' @param file_names a list of files with particular names (i.e. Sample_A.txt). If you leave it empty, it will return all dataset files under the dataset ID.
 #' @param full_path Boolean. If True return the full path as part of this filename; otherwise return just base filename
-#' @param input_token synapse PAT
-#'
+#' @param url URL to schematic API endpoint
+#' 
 #' @export
 
-storage_dataset_files <- function(url="http://localhost:3001/v1/storage/dataset/files",
-                                  asset_view,
-                                  dataset_id, file_names=list(),
-                                  full_path=FALSE, input_token) {
+storage_dataset_files <- function(asset_view,
+                                  dataset_id,
+                                  input_token,
+                                  file_names=list(),
+                                  full_path=FALSE, 
+                                  url="http://localhost:3001/v1/storage/dataset/files") {
   
   req <- httr::GET(url,
                    #add_headers(Authorization=paste0("Bearer ", pat)),
