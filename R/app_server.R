6a1fa077b251cc265fb86c5cbdf7be6f565226e8
lakikowolfe <lauren.wolfe@sagebase.org>
2022-05-13 10:39:41 -0700

    complete manifest modification reactive expression

--- R/app_server.R
+++ R/app_server.R
@@ -32,56 +32,62 @@ app_server <- function( input, output, session ) {
   # MOD_SET_STATUS
   release_status_selection <- mod_set_release_status_server("set_release_status_ui_1")
   
-  # manifest_mod <- reactive({
-  #   
-  #   mani <- file_selection$manifest()
-  #   
-  #   # check manifest for status column
-  #   # if column doesn't exist, add it
-  #   
-  #   status_col <- "ReleaseStatus"
-  #   
-  #   if (!status_col %in% colnames(mani)) {
-  #     mani$x <- NA
-  #     names(mani)[names(mani) == "x"] <- status_col
-  #     } 
-  #   
-  #   # add release_status_selection to selected rows
-  #   mani[file_selection$selected_rows(), status_col] <- release_status_selection$status_selection()
-  #   
-  #   return(mani)
-  # 
-  # })
+  # modify selected manifest rows with release status selection
+  manifest_mod <- reactive({
+
+    mani <- file_selection$manifest()
+
+    # check manifest for status column
+    # if column doesn't exist, add it
+
+    status_col <- "ReleaseStatus"
+
+    if (!status_col %in% colnames(mani)) {
+      mani$x <- NA
+      names(mani)[names(mani) == "x"] <- status_col
+      }
+
+    # add release_status_selection to selected rows
+    mani[file_selection$selected_rows(), status_col] <- release_status_selection$status_selection()
+
+    return(mani)
+
+  })
   
   # wait for button click to display table
   # in place of model/submit endpoint for now
   observeEvent(release_status_selection$btn_click(), {
     
+    # create manifest dir
+    suppressWarnings(dir.create("./manifest"))
+    
+    # write modified manifest to dir
+    write.table(manifest_mod(),
+                "./manifest/synapse_storage_manifest.csv",
+                sep = ",",
+                row.names = FALSE)
+    
+    # submit to symapse
     model_submit(data_type = "None", 
                  dataset_id = dataset_selection()$id,
                  restrict_rules = FALSE,
-                 csv_file = "~/Desktop/tsting_manifests/unmodified_manifest/synapse_storage_manifest.csv",
+                 csv_file = "~/Desktop/synapse_storage_manifest.csv",
                  input_token = schematic_token,
                  manifest_record_type = "table",
                  url="http://localhost:3001/v1/model/submit",
                  schema_url="https://raw.githubusercontent.com/ncihtan/data-models/main/HTAN.model.jsonld")
     
-    output$modified_manifest <- DT::renderDataTable({
-      DT::datatable(file_selection$manifest(),
-                    option = list(scrollY = 500,
-                                  scrollX = TRUE,
-                                  scrollCollapse = TRUE,
-                                  bPaginate = FALSE,
-                                  dom = "t"),
-                    filter = list(position = 'top', clear = TRUE))
-      
-      # write.table(manifest_mod(), 
-      #             "~/Desktop/synapse_storage_manifest.csv", 
-      #             sep = ",",
-      #             row.names = FALSE)
-      
-      
-    })
+    # for testing manifest modification
+    # output$modified_manifest <- DT::renderDataTable({
+    #   DT::datatable(manifest_mod(),
+    #                 option = list(scrollY = 500,
+    #                               scrollX = TRUE,
+    #                               scrollCollapse = TRUE,
+    #                               bPaginate = FALSE,
+    #                               dom = "t"),
+    #                 filter = list(position = 'top', clear = TRUE))
+    # })
+    
   })
   
   
