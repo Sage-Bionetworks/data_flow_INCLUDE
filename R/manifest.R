7ae78f6210f1ddd567cf62bd278ca3f3354cc1f7
lakikowolfe <lauren.wolfe@sagebase.org>
2022-11-09 09:40:19 -0800

    move update_dfs_manifest to manifest.R

--- R/manifest.R
+++ R/manifest.R
@@ -54,29 +54,47 @@ prep_manifest_submit <- function(manifest,
   return(manifest)
 }
 
+#' Convert a list to a dataframe
+#'
+#' @param dfs_status_manifest A data flow status manifest
+#' @param dfs_updates Output from mod_update_data_flow_status.R
+#' @param selected_datasets_df Output from mod_dataset_selection.R
+#' 
+#' @export
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+update_dfs_manifest <- function(dfs_manifest,
+                                dfs_updates,
+                                selected_datasets_df) {
+  
+  # remove unchanged attributes from selections
+  dfs_updates <- dfs_updates[!unlist(lapply(dfs_updates, is.null))]
+  
+  # capture column names to update
+  col_names <- names(dfs_updates)
+  
+  # loop over the list of changed attributes
+  # for each attribute:
+  #   - pull out the original vector
+  #   - get the updated entry from the list of attributes
+  #   - apply the entry to the selected datasets in dfs manifest
+  dfs_manifest[col_names] <- lapply(col_names, function(x) {
+    
+    # pull out column into a vector
+    vec <- dfs_manifest[[x]]
+    
+    # get entry from updated data flow status attributes list
+    entry <- dfs_updates[[x]]
+    
+    # update vector by index
+    manifest_selected_idx <- match(selected_datasets_df$id, dfs_manifest$entityId)
+    vec[manifest_selected_idx] <- entry
+    
+    return(vec)
+    
+  })
+  
+  return(dfs_manifest)
+}
 
 #' Generate a data flow status manifest. Fills in the component, contributor, data type, and dataset name columns.
 #' 
@@ -167,41 +185,3 @@ generate_data_flow_manifest <- function(storage_project_id,
   
   return(df)
 }
-
-#' Convert date columns from string to date
-#'
-#' @param manifest A data flow status manifest. 
-#' 
-#' @export
-
-manifest_string_to_date <- function(manifest) {
-  
-  # convert Not Applicable to NA
-  manifest[ manifest == "Not Applicable" ] <- NA
-  
-  # convert string to date for date cols
-  date_cols <- c("release_scheduled", "embargo")
-  manifest[date_cols] <- lapply(manifest[,date_cols], as.Date)
-  
-  return(manifest)
-}
-
-#' Convert date columns from date to string
-#'
-#' @param manifest a data flow status manifest.
-#' 
-#' @export
-
-manifest_date_to_string <- function(manifest) {
-  
-  # convert date to string for date cols
-  date_cols <- c("release_scheduled", "embargo")
-  manifest[date_cols] <- lapply(manifest[,date_cols], as.character)
-  
-  # convert NA to Not Applicable
-  manifest[ is.na(manifest) ] <- "Not Applicable"
-  
-  return(manifest)
-}
-
-
