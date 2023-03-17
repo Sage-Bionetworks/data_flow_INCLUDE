6cf10f12db218083a66f419410110cb37dbbd03f
lakikowolfe <lauren.wolfe@sagebase.org>
2023-03-17 15:27:01 -0700

    function that gets all manifests under an asset view

--- R/api_wrappers.R
+++ R/api_wrappers.R
@@ -44,3 +44,72 @@ manifest_download_to_df <- function(asset_view,
   return(content_df)
 }
 
+#' Call `storage/project/manifests` Schematic endpoint for a given list of storage projects (output from `storage/projects`) and output information as a dataframe
+#' 
+#' @param asset_view ID of view listing all project data assets. For example, for Synapse this would be the Synapse ID of the fileview listing all data assets for a given project.(i.e. master_fileview in config.yml)
+#' @param input_token Synapse PAT
+#' @param base_url Base URL of schematic API
+#' @param verbose T/F for console messages
+#' 
+#' @export
+
+get_all_manifests <- function(asset_view,
+                              input_token,
+                              base_url = "https://schematic-dev.api.sagebionetworks.org",
+                              verbose = FALSE) {
+  if (verbose) {
+    message(paste0("Getting storage project list for ", asset_view))
+  }
+  
+  # get all storage projects under asset view
+  sp <- dataflow::storage_projects(asset_view = asset_view,
+                                   input_token = input_token,
+                                   base_url = base_url)
+  
+  if (verbose) {
+    message(paste0("Getting manifests for ", length(sp), " storage project(s)"))
+  }
+  
+  synapse_manifests_list <- lapply(1:length(sp), function(i) {
+    Sys.sleep(.1)
+    if (verbose) {
+      storage_project <- purrr::map_chr(sp[i], 2)
+      
+      message(glue::glue("Retrieving manifests for {storage_project}"))
+    }
+    
+    sp_id <- purrr::map_chr(sp[i], 1)
+    
+    manifest <- dataflow::storage_project_manifests(asset_view = asset_view,
+                                                    project_id = sp_id,
+                                                    input_token = input_token,
+                                                    base_url = base_url)
+    
+    
+    # if manifest has
+    if (length(manifest) > 0) {
+      
+      # pull out data from list
+      # dataset metadata
+      dataset_naming_metadata <- purrr::map(manifest, 1)
+      
+      # get manifest synid to ID rows with no manifest
+      dataset_manifest_metadata <- purrr::map(manifest, 2)
+      
+      # component metadata 
+      dataset_component_metadata <- purrr::map(manifest, 3)
+      
+      # pull together in a dataframe
+      return(data.frame(Component = rep("DataFlow", length(manifest)),
+                        contributor = rep(purrr::map_chr(sp[i], 2), length(manifest)),
+                        entityId = purrr::map_chr(dataset_naming_metadata, 1),
+                        dataset_name = purrr::map_chr(dataset_naming_metadata, 2),
+                        dataset = purrr::map_chr(dataset_component_metadata, 1)))
+    } else {
+      return(NULL)
+    }
+  })
+  
+  # return dataframe
+  return(do.call("rbind", synapse_manifests_list))
+}
