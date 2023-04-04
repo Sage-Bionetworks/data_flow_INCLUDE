840129919f4d28a68e25bad6de71f4ed989e6c9b
lakikowolfe <lauren.wolfe@sagebase.org>
2023-04-04 14:16:01 -0700

    add visualize_component test

--- tests/testthat/test-schematic-api.R
+++ tests/testthat/test-schematic-api.R
@@ -3,9 +3,9 @@
 
 # VARIABLES #############################################################################
 # FAIR DEMO DATA PROJECT A
-asset_view <- "syn50896957"
-project_id <- "syn50896931"
-dataset_id <- "syn51219090"
+asset_view <- "syn50896957" # FAIR Demo All Projects, Files and Folders
+project_id <- "syn50896931" # FAIR Demo Project A
+dataset_id <- "syn51219090" # DataFlowStatusDFATesting
 input_token <- Sys.getenv("SYNAPSE_PAT")
 base_url <- Sys.getenv("SCHEMATIC_BASE_URL_AWS")
 testing_manifest_path <- "../../inst/testing/synapse_storage_manifest_dataflow.csv"
@@ -14,36 +14,36 @@ schema_url <- "https://raw.githubusercontent.com/Sage-Bionetworks/data_flow/main
 # TEST API ##############################################################################
 
 test_that("storage_projects successfully returns a schematic_api object", {
-  sp <- try(storage_projects(asset_view = asset_view,
+  res <- try(storage_projects(asset_view = asset_view,
                              input_token = input_token),
             silent = FALSE)
   
-  expect_true(class(sp) == "schematic_api")
+  expect_true(class(res) == "schematic_api")
 })
 
 test_that("storage_project_datasets successfully returns a schematic_api object", {
-  spd <- try(storage_project_datasets(asset_view = asset_view,
+  res <- try(storage_project_datasets(asset_view = asset_view,
                                       project_id = project_id,
                                       input_token = input_token,
                                       base_url = base_url),
              silent = FALSE)
   
-  expect_true(class(spd) == "schematic_api")
+  expect_true(class(res) == "schematic_api")
 })
 
 test_that("manifest_download successfully returns a schematic_api object", {
-  md <- try(manifest_download(input_token = input_token,
+  res <- try(manifest_download(input_token = input_token,
                               asset_view = asset_view,
                               dataset_id = dataset_id,
                               base_url = base_url),
             silent = FALSE)
   
-  expect_true(class(md) == "schematic_api")
+  expect_true(class(res) == "schematic_api")
   
 })
 
 test_that("model_submit successfully returns a schematic_api object", {
-  s <- try(model_submit(data_type = NULL, 
+  res <- try(model_submit(data_type = NULL, 
                         asset_view = asset_view,
                         dataset_id = dataset_id,
                         file_name = testing_manifest_path,
@@ -55,16 +55,26 @@ test_that("model_submit successfully returns a schematic_api object", {
                         use_schema_label = TRUE),
            silent = FALSE)
   
-  expect_true(class(s) == "schematic_api")
+  expect_true(class(res) == "schematic_api")
 })
 
 test_that("storage_project_manifests successfully returns a schematic_api object", {
-  spm <- try(storage_project_manifests(asset_view,
+  res <- try(storage_project_manifests(asset_view,
                                       project_id,
                                       input_token,
                                       base_url),
             silent = FALSE)
   
-  expect_true(class(spm) == "schematic_api")
+  expect_true(class(res) == "schematic_api")
+  
+})
+
+test_that("visualize/component successfully returns a schematic_api object", {
+  res <- try(visualize_component(schema_url = schema_url,
+                                 component = "DataFlow",
+                                 base_url = base_url),
+             silent = FALSE)
+  
+  expect_true(class(res) == "schematic_api")
   
 })
